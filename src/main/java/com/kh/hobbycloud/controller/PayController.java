dcff1a6c8e7e50e97abb90e8c9abf93c190b7686
Daanta <daanta@naver.com>
2021-12-27 14:09:57 +0900

    211227_1409 카카오페이 응답VO 생성

--- src/main/java/com/kh/hobbycloud/controller/PayController.java
+++ src/main/java/com/kh/hobbycloud/controller/PayController.java
@@ -2,9 +2,31 @@ package com.kh.hobbycloud.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/pay")
 public class PayController {
+
+	// 카카오페이 진입 페이지
+	@ResponseBody
+	@RequestMapping("/")
+	public String index() {
+		return "진입";
+	}
+
+	// 카카오페이 성공 페이지
+	@ResponseBody
+	@RequestMapping("/success")
+	public String success() {
+		return "성공";
+	}
+
+	// 카카오페이 실패 페이지
+	@ResponseBody
+	@RequestMapping("/fail")
+	public String fail() {
+		return "실패";
+	}
 
 }
