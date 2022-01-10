1a77252d45661473b7857ef740a63d23f2a7ee47
Kangsunmo3230 <ksm0212000@naver.com>
2022-01-10 10:48:44 +0900

    0110 update

--- src/main/java/com/kh/hobbycloud/vo/gather/UserVO.java
+++ src/main/java/com/kh/hobbycloud/vo/gather/UserVO.java
@@ -3,17 +3,15 @@ package com.kh.hobbycloud.vo.gather;
 import org.springframework.web.socket.WebSocketSession;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
 //@EqualsAndHashCode(of = {"userId", "session"})
 public class UserVO {
-	private String userId;
-	private WebSocketSession session;
 	
-	public boolean isMember() {
-		return userId != null;
-	}
-	public boolean isGuest() {
-		return userId == null;
-	}
+	
+	private String memberIdx;
+	
+	private WebSocketSession session;
+
 }
