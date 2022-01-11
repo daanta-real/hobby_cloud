17f49bcadabdc5c159d019d667c44f23e63cb97d
Daanta <daanta@naver.com>
2022-01-12 08:23:02 +0900

    220112 JUnit 실행불가 문제 해결을 위한 웹소켓 작동 방지 설정

--- src/main/java/com/kh/hobbycloud/websocket/BasicWebsocketServer.java
+++ src/main/java/com/kh/hobbycloud/websocket/BasicWebsocketServer.java
@@ -1,22 +1,15 @@
 package com.kh.hobbycloud.websocket;
 
-import java.util.Set;
-import java.util.concurrent.CopyOnWriteArraySet;
-
-import org.springframework.web.socket.CloseStatus;
-import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
-
-import com.kh.hobbycloud.vo.gather.UserVO;
 
 import lombok.extern.slf4j.Slf4j;
 
 
 /**
- * 기본 웹소켓 서버 
- * 	= 상속은 인터페이스 or 클래스 선택하여 받는다 
+ * 기본 웹소켓 서버
+ * 	= 상속은 인터페이스 or 클래스 선택하여 받는다
  * 	= 기본 접속은 모두 Spring-Websocket 모듈에서 관리한다
- * 	= 서버로서 반드시 가져야 할 기능들을 재정의하여 웹소켓 사용자들을 관리한다 
+ * 	= 서버로서 반드시 가져야 할 기능들을 재정의하여 웹소켓 사용자들을 관리한다
  * 		= 입장알림(afterConnectionEstablished) - 사용자가 접속한 이후에 사용자의 정보를 알려주는 메소드
  * 		= 퇴장알림(afterConnectionClosed) - 사용자의 접속 종료 이후에 사용자의 정보를 알려주는 메소드
  * 		= 메세지
@@ -24,8 +17,8 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 //public class BasicWebsocketServer implements WebSocketHandler{
 public class BasicWebsocketServer extends TextWebSocketHandler {
-	
-	@Override
+
+	/*@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
 		log.debug("사용자가 입장했습니다");
 		log.debug("session = {}", session);
@@ -36,6 +29,6 @@ public class BasicWebsocketServer extends TextWebSocketHandler {
 		log.debug("사용자가 퇴장했습니다");
 		log.debug("session = {}", session);
 		log.debug("status = {}", status);
-	}
+	}*/
 
 }
