83ae9ab7a59f0c0ec6659719dc9289cfcbc7c1f2
Kangsunmo3230 <ksm0212000@naver.com>
2022-01-14 17:33:43 +0900

    비회원 인터셉터 적용

--- src/main/java/com/kh/hobbycloud/interceptor/MemberInterceptor.java
+++ src/main/java/com/kh/hobbycloud/interceptor/MemberInterceptor.java
@@ -10,13 +10,16 @@ public class MemberInterceptor implements HandlerInterceptor{
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-		String memberId = (String) request.getSession().getAttribute("memberId");
-		boolean isLogin = memberId != null;
+		String memberNick = (String)request.getSession().getAttribute("memberNick");
+		
+		boolean isLogin = memberNick != null;    
 		if(isLogin) {
 			return true;
 		}
 		else {
-			response.sendError(401);
+			//response.sendError(401);
+			//비회원은 차단
+			response.sendRedirect(request.getContextPath()+"/member/login"); //리다이렉트처리
 			return false;
 		}
 	}
