9485b8b78b48a113ff73d7252b5217121d443382
Daanta <daanta@naver.com>
2022-01-15 09:37:43 +0900

    220115_0936 lec 다중 카테고리 검색 + 다중 지역 검색 + 페이지네이션 구현, UI 오류 해결

--- src/main/java/com/kh/hobbycloud/vo/lec/LecSearchVO.java
+++ src/main/java/com/kh/hobbycloud/vo/lec/LecSearchVO.java
@@ -10,7 +10,7 @@ public class LecSearchVO {
 	private List<String> lecCategoryName;
 	private String lecName;
 	private String memberNick;
-	private String lecLocRegion;
+	private List<String> lecLocRegion;
 	private String minPrice;
 	private String maxPrice;
 	private String minCount;
